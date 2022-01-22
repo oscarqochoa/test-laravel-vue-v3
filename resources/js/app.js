022c01ec6ab19a78fa907d1f34e59ef6a23ac416
Oscar Quiroz <oscarqochoa@gmail.com>
2022-01-22 09:18:46 -0500

    First commit

---
+++ resources/js/app.js
@@ -0,0 +1,22 @@
+require('./bootstrap');
+import Vue from 'vue/dist/vue'
+
+import App from './components/App.vue';
+import VueAxios from 'vue-axios';
+import VueRouter from 'vue-router';
+import axios from 'axios';
+import { routes } from './routes';
+
+Vue.use(VueRouter);
+Vue.use(VueAxios, axios);
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: routes
+});
+
+const app = new Vue({
+    el: '#app',
+    router: router,
+    render: h => h(App),
+});
